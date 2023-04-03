92936e4162db3bfdcbff8b0ad2ce4ef16d8427a8
TheFluxx <zhemerov17@mail.ru>
2023-04-03 15:00:02 +0300

    project

---
+++ backend/tags_ingr/models.py
@@ -0,0 +1,44 @@
+from django.core.validators import RegexValidator
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+    )
+    color = models.CharField(
+        max_length=7,
+        unique=True
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^[-a-zA-Z0-9_]+$',
+                message='Только латинские буквы и символы "-" "_"',
+            ),
+        ]
+    )
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    measurement_unit = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return str(self.name)
